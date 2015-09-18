2ab14db9893171876839c7ac813e8042e19bf217
jingfei <jingfei955047@gmail.com>
2015-09-17 22:30:06 -0700

    do homework #1

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -13,30 +13,29 @@ fibonacci:
 	@ PROLOG
 	push {r3, r4, r5, lr}
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+	sub r4, r0, #0 @ R4 = R0 - 0 (update flags)
+	sub r3, r1, #0 @ first
+	sub r5, r2, #0 @ second
+	cmp r0, #0 @ if(R0 <= 0) goto .L3 (which returns second)
+	ble .L3
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+	cmp r4, #1 @ Compare R4 wtih 1
+	beq .L4  @ If R4 == 1 goto .L4 (which returns first)
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
+	sub r0, r4, #1 @ R0 = R4 - 1
+	add r1, r3, r5 @ R1 = first+second
+	mov r2, r3     @ R2 = first
+	bl fibonacci @ Recursive call to fibonacci 
 
 	pop {r3, r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
 .L3:
-	mov r0, #0			@ R0 = 0
+	mov r0, r5			@ R0 = second
 	pop {r3, r4, r5, pc}		@ EPILOG
 
 .L4:
-	mov r0, #1			@ R0 = 1
+	mov r0, r3			@ R0 = first
 	pop {r3, r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
