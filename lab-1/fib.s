ba5d66355298158ab44a3266819d27df6b576997
jingfei <jingfei955047@gmail.com>
2015-09-22 17:12:10 -0700

    Change subroutine's pushed registers, according to AAPCS
    Core registers and AAPCS usage
     * R0 ~ R3: Arguments
     * R4 ~ R8, R10, R11: Variable register
     * R9: Platform register
     * R12: IP (The Intra-Procedure-call scratch register)
     * R13: SP (The Stack Pointer)
     * R14: LR (The Link Register)
     * R15: PC (The Program Counter)

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -11,25 +11,25 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, lr}
+	push {r4, r5, lr}
 
 	subs r4, r0, #0 	@ R4 = R0 - 0 (update flags)
 	beq .L4  			@ If R4 == 0 goto .L4 (which returns second)
 
-	sub r3, r1, #0 		@ R3 = first
+	sub r5, r1, #0 		@ R5 = first
 
 	sub r0, r4, #1 		@ R0 = R4 - 1
-	add r1, r3, r2 		@ R1 = first+second
-	mov r2, r3     		@ R2 = first
+	add r1, r5, r2 		@ R1 = first+second
+	mov r2, r5     		@ R2 = first
 	bl fibonacci 		@ Recursive call to fibonacci 
 
-	pop {r3, r4, pc}		@EPILOG
+	pop {r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
 
 .L4:
 	sub r0, r2, #0				@ R0 = second
-	pop {r3, r4, pc}			@ EPILOG
+	pop {r4, r5, pc}			@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -11,20 +11,20 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, lr}
+	push {r4, r5, r6, lr}
 
 	sub r4, r0, #0 	@ R4 = R0 - 0 (update flags)
 
-	mov r3, #0		@ init fib(0) = 0
-	mov r5, #1		@ init fib(1) = 1
-.L5:
-	mov r0, r5		@ R0 = R5 and also the result
-	add r5, r3, r5	@ R5 += R3
-	mov r3, r0		@ R3 = R0   (exchange R5, R3)
+	mov r5, #0		@ init fib(0) = 0
+	mov r6, #1		@ init fib(1) = 1
+.Loop:
+	mov r0, r6		@ R0 = R6 and also the result
+	add r6, r5, r6	@ R6 += R5
+	mov r5, r0		@ R5 = R0   (exchange R5, R6)
 	subs r4, r4, #1 	@ R4 = R4 - 1
-	bne .L5
+	bne .Loop
 
-	pop {r3, r4, r5, pc}		@EPILOG
+	pop {r4, r5, r6, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
 
